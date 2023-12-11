ac4e445c75af377b3218eb33164c147313cb6ce1
firdaus nanda putra <firdaus.nandaputra@ebconnect.com>
2023-12-12 01:36:27 +0700

    set pagination in cluster page

--- controllers/Cluster.controller.js
+++ controllers/Cluster.controller.js
@@ -11,9 +11,12 @@ const INTERNAL_SERVER_ERROR = 500;
 const { JSDOM } = jsdom;
 
 async function clusterPage(req, res) {
-    const page = req.query.page || 1;
-    const size = req.query.size || 10;
+    const page = parseInt(req.query.page || 1);
+    const size = parseInt(req.query.size || 10);
     const key = req.query.q || null;
+
+    let num = 0;
+    let pageNumber = "";
 
     try {
         let where = {};
@@ -21,12 +24,16 @@ async function clusterPage(req, res) {
             where.name = { [Op.substring]: key }
         }
         const { count, rows } = await clusterServ.getAllCluster(where, page, size);
-        let { number, pageNumUi } = pagination.setPagination(rows, count, page, size, key, "/metroland/auth/clusters");
+        if (count > 0) {
+            let { number, pageNumUi } = pagination.setPagination(rows, count, page, size, key, "/metroland/auth/clusters");
+            num = number;
+            pageNumber = pageNumUi;
+        }
         const facilities = await facilityService.getAllFacilities();
         // console.log("--> " + JSON.stringify(rows))
 
         res.render("clusters/index", {
-            title, header, clusters: rows, moment, csrfToken: req.csrfToken(), JSDOM, facilities, pagination: pageNumUi
+            title, header, clusters: rows, moment, csrfToken: req.csrfToken(), JSDOM, facilities, pagination: pageNumber, no: num
         });
     } catch (error) {
         console.error(`err clusterPage : ${error}`)
--- helpers/Pagination.js
+++ helpers/Pagination.js
@@ -158,7 +158,7 @@ function pagination(data) {
     const total_perpage = data.per_page;
     const current_page = data.page;
     const previos_page = current_page == 1 ? null : current_page - 1;
-    const next_page = current_page == total_page ? null : current_page + 1;
+    const next_page = current_page == total_page ? total_page : current_page + 1;
 
     const result = {
         data: data.data,
